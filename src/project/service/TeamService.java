4efc098e13096608d7b8226020fecc152465c69e
Ayon <ayenuddin.bhuiyan@gmail.com>
2026-04-20 23:44:21 +0600

    Add files via upload

---
+++ src/project/service/TeamService.java
@@ -0,0 +1,34 @@
+package project.service;
+
+import project.model.Team;
+import java.util.ArrayList;
+
+public class TeamService {
+
+    private ArrayList<Team> teams = new ArrayList<>();
+
+    public void add(Team team) {
+        teams.add(team);
+    }
+
+    public Team findById(int id) {
+        for (Team t : teams) {
+            if (t.getTeamId() == id) {
+                return t;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return teams.isEmpty();
+    }
+
+    public void showTeams() {
+        System.out.println("--- Available Teams ---");
+        for (Team t : teams) {
+            System.out.println("[ " + t.getTeamId() + " ] " + t.getName());
+        }
+    }
+}
